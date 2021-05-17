f1cbd8b266d5bb9327d42fe2d80d6350b5e2b3c8
Johnny Hacking <jhacking@bhico.com>
2021-05-17 07:04:14 -0600

    Setup IBAction for number buttons

--- CalculatorProject/Controller/ViewController.swift
+++ CalculatorProject/Controller/ViewController.swift
@@ -10,14 +10,38 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var resultsLabel: CPResultsLabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.4535933514)
         
-        
     }
-
+    @IBAction func numButtonPressed(_ sender: UIButton){
+        switch sender.tag {
+        case 1:
+            print("1")
+        case 2:
+            print("2")
+        case 3:
+            print("3")
+        case 4:
+            print("4")
+        case 5:
+            print("5")
+        case 6:
+            print("6")
+        case 7:
+            print("7")
+        case 8:
+            print("8")
+        case 9:
+            print("9")
+        default:
+            print("0")
+        }
+    }
 
 }
 
