f1e6982372c580d070b8a9db5526ac480f861495
Johnny Hacking <jhacking@bhico.com>
2021-05-18 07:59:25 -0600

    Revert "Connected my buttons to IBActions"
    This reverts commit b3e5f9707c05860eea29e3a0c7c6a603d08d0705.

--- CalculatorProject/Controller/ViewController.swift
+++ CalculatorProject/Controller/ViewController.swift
@@ -12,65 +12,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.4535933514)
         
         
     }
-    //MARK: - TopLevelButtons
-    @IBAction func clearButtonPressed(_ sender: Any) {
-    }
-    
-    @IBAction func plusMinusButtonPressed(_ sender: Any) {
-    }
-    
-    @IBAction func divisonButtonPressed(_ sender: Any) {
-    }
-    
-    @IBAction func multiButtonPressed(_ sender: Any) {
-    }
-   //MARK: - Operation Buttons
-    @IBAction func minusButtonPressed(_ sender: Any) {
-    }
-    
-    @IBAction func additionButtonPressed(_ sender: Any) {
-    }
-    
-    @IBAction func equalButtonPressed(_ sender: Any) {
-    }
-    //MARK: - Number Buttons
-    
-    @IBAction func zeroButtonPressed(_ sender: Any) {
-    }
-    
-    @IBAction func oneButtonPressed(_ sender: Any) {
-    }
-    
-    @IBAction func twoButtonPressed(_ sender: Any) {
-    }
-    
-    @IBAction func threeButtonPressed(_ sender: Any) {
-    }
-    
-    @IBAction func fourButtonPressed(_ sender: Any) {
-    }
-    
-    @IBAction func fiveButtonPressed(_ sender: Any) {
-    }
-    
-    @IBAction func sixButtonPressed(_ sender: Any) {
-    }
-    
-    @IBAction func sevinButtonPressed(_ sender: Any) {
-    }
-    
-    @IBAction func eightButtonPressed(_ sender: Any) {
-    }
-    
-    @IBAction func nineButtonPressed(_ sender: Any) {
-    }
-    
-    @IBAction func periodButtonPressed(_ sender: Any) {
-    }
-    
+
+
 }
 
